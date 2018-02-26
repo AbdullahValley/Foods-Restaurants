df5d4671c5ff95fa73c3fe562bb489d5e2529007
Abdullah Al Noman <noman19bd@yahoo.com>
2018-02-26 07:46:11 +0600

    intervation added for images

--- config/app.php
+++ config/app.php
@@ -150,6 +150,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -208,6 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 

260d8837d46f14428851f77cc9fbd3d30412b6b7
Abdullah Al Noman <noman19bd@yahoo.com>
2018-04-03 17:38:47 +0600

    Sidebar Updated

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,9 +3,7 @@
         <ul class="nav side-menu">
             <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
-                    <li><a href="index.html">Dashboard</a></li>
-                    <li><a href="index2.html">Dashboard2</a></li>
-                    <li><a href="index3.html">Dashboard3</a></li>
+                    <li><a href="{{ url('/food-admin') }}">Dashboard</a></li>
                 </ul>
             </li>
             <li><a><i class="fa fa-user"></i> User <span class="fa fa-chevron-down"></span></a>
@@ -23,94 +21,127 @@
                     <li><a href="{{ url('/food-admin/role-permission') }}">Add Role & Permission</a></li>
                 </ul>
             </li>
-
-            <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="form.html">General Form</a></li>
-                    <li><a href="form_advanced.html">Advanced Components</a></li>
-                    <li><a href="form_validation.html">Form Validation</a></li>
-                    <li><a href="form_wizards.html">Form Wizard</a></li>
-                    <li><a href="form_upload.html">Form Upload</a></li>
-                    <li><a href="form_buttons.html">Form Buttons</a></li>
-                </ul>
-            </li>
-            <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="general_elements.html">General Elements</a></li>
-                    <li><a href="media_gallery.html">Media Gallery</a></li>
-                    <li><a href="typography.html">Typography</a></li>
-                    <li><a href="icons.html">Icons</a></li>
-                    <li><a href="glyphicons.html">Glyphicons</a></li>
-                    <li><a href="widgets.html">Widgets</a></li>
-                    <li><a href="invoice.html">Invoice</a></li>
-                    <li><a href="inbox.html">Inbox</a></li>
-                    <li><a href="calendar.html">Calendar</a></li>
-                </ul>
-            </li>
-            <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="tables.html">Tables</a></li>
-                    <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                </ul>
-            </li>
-            <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="chartjs.html">Chart JS</a></li>
-                    <li><a href="chartjs2.html">Chart JS2</a></li>
-                    <li><a href="morisjs.html">Moris JS</a></li>
-                    <li><a href="echarts.html">ECharts</a></li>
-                    <li><a href="other_charts.html">Other Charts</a></li>
-                </ul>
-            </li>
-            <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-                    <li><a href="fixed_footer.html">Fixed Footer</a></li>
-                </ul>
-            </li>
         </ul>
 </div>
 
     <div class="menu_section">
-        <h3>Live On</h3>
+        <h3>Restaurant</h3>
         <ul class="nav side-menu">
-            <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
+            <li><a><i class="fa fa-bug"></i> Restaurant Types <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
-                    <li><a href="e_commerce.html">E-commerce</a></li>
-                    <li><a href="projects.html">Projects</a></li>
-                    <li><a href="project_detail.html">Project Detail</a></li>
-                    <li><a href="contacts.html">Contacts</a></li>
-                    <li><a href="profile.html">Profile</a></li>
+                    <li><a href="#">Add Type</a></li>
+                    <li><a href="#">All Types</a></li>
                 </ul>
             </li>
-            <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
+            <li><a><i class="fa fa-bug"></i> Restaurant Categories <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
-                    <li><a href="page_403.html">403 Error</a></li>
-                    <li><a href="page_404.html">404 Error</a></li>
-                    <li><a href="page_500.html">500 Error</a></li>
-                    <li><a href="plain_page.html">Plain Page</a></li>
-                    <li><a href="login.html">Login Page</a></li>
-                    <li><a href="pricing_tables.html">Pricing Tables</a></li>
+                    <li><a href="#">Add Category</a></li>
+                    <li><a href="#">All Categories</a></li>
                 </ul>
             </li>
-            <li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>
+            <li><a><i class="fa fa-bug"></i> Restaurant Reviews <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
-                    <li><a href="#level1_1">Level One</a>
-                    <li><a>Level One<span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li class="sub_menu"><a href="level2.html">Level Two</a>
-                            </li>
-                            <li><a href="#level2_1">Level Two</a>
-                            </li>
-                            <li><a href="#level2_2">Level Two</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="#level1_2">Level One</a>
-                    </li>
+                    <li><a href="#">Add Review</a></li>
+                    <li><a href="#">All Reviews</a></li>
                 </ul>
             </li>
-            <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
+            <li><a><i class="fa fa-bug"></i> Restaurant Comments <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Pending Comments</a></li>
+                    <li><a href="#">All Comments</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Restaurant Facilities <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add Facility</a></li>
+                    <li><a href="#">All Facilities</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Restaurants <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add Restaurant</a></li>
+                    <li><a href="#">All Restaurants</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Restaurant News <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add News</a></li>
+                    <li><a href="#">All News</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Restaurant Compares <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Compare Status</a></li>
+                    <li><a href="#">Top Compares</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+
+    <div class="menu_section">
+        <h3>Food</h3>
+        <ul class="nav side-menu">
+            <li><a><i class="fa fa-bug"></i> Food Courts <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add Court</a></li>
+                    <li><a href="#">All Courts</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Food Groups <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add Group</a></li>
+                    <li><a href="#">All Groups</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Food Reviews <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add Review</a></li>
+                    <li><a href="#">All Reviews</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Food Comments <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Pending Comments</a></li>
+                    <li><a href="#">All Comments</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Foods <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add Food</a></li>
+                    <li><a href="#">All Foods</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Food News <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add News</a></li>
+                    <li><a href="#">All News</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Food Compares <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Compare Status</a></li>
+                    <li><a href="#">Top Compares</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+    <div class="menu_section">
+        <h3>Location</h3>
+        <ul class="nav side-menu">
+            <li><a><i class="fa fa-bug"></i> Location <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add Location</a></li>
+                    <li><a href="#">All Locations</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-bug"></i> Sub Location <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="#">Add Sub Location</a></li>
+                    <li><a href="#">All Sub Locations</a></li>
+                </ul>
+            </li>
         </ul>
     </div>
 
